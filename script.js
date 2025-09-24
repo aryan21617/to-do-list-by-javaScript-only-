871607b5a36ec77f0eb4ba5e65b974cf1f54ad51
aryan21617 <aryanpurbey1617@gmail.com>
2025-09-24 22:41:19 +0530

    Added Everything

--- script.js
+++ script.js
@@ -20,5 +20,14 @@ document.querySelector('#push').onclick = function() {
         this.parentNode.remove();
       }
     }
+
+    var tasks = document.querySelectorAll(".task");
+    for(var i=0; i<tasks.length; i++){
+      tasks[i].onclick = function(){
+        this.classList.toggle('completed');
+      }
+    }
+
+    document.querySelector('#newtask input').value = "";
   }
 }
